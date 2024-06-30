f129a08c97c837e61a086fbba9cd98ea74e77c65
arth <piyushmishra780720@gmail.com>
2024-06-30 20:52:26 +0530

    add logic and country info api

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CountryInformation from './components/CountryInformation'
+
+const App = () => {
+  return (
+    <div>
+      <CountryInformation/>
+    </div>
+  )
+}
+
+export default App
